d151a2e67771227b744491f2655ecf66056583d0
Tung Ngo <tungthanh254@gmail.com>
2025-06-02 15:16:24 +0700

    Enhance CompanionsList component to display recent sessions in a table format, update main page to include this list, and improve CTA section with additional content and styling

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -1,9 +1,106 @@
-import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { cn, getSubjectColor } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 
-const CompanionsList = () => {
-  return (
-    <div>CompanionsList</div>
-  )
+interface CompanionsListProps {
+  title: string;
+  companions: Companion[];
+  classNames?: string;
 }
 
-export default CompanionsList
+const CompanionsList = ({
+  title,
+  companions,
+  classNames,
+}: CompanionsListProps) => {
+  return (
+    <article className={cn("companion-list", classNames)}>
+      <h2 className="font-bold text-3xl">Recent Sessions</h2>
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="text-lg w-2/3">Lessons</TableHead>
+            <TableHead className="text-lg">Subject</TableHead>
+            <TableHead className="text-lg">Duration</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {companions.map(({ id, subject, name, topic, duration }) => (
+            <TableRow key={id}>
+              <TableCell>
+                <Link href={`/companions/${id}`}>
+                  <div
+                    className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden"
+                    style={{ backgroundColor: getSubjectColor(subject) }}
+                  >
+                    <Image
+                      src={`/icons/${subject}.svg`}
+                      alt={subject}
+                      width={35}
+                      height={35}
+                    />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <p className="text-2xl font-bold">{name}</p>
+                    <p className="text-lg">{topic}</p>
+                  </div>
+                </Link>
+              </TableCell>
+
+              <TableCell>
+                <div className="subject-badge w-fit max-md:hidden">
+                  {subject}
+                </div>
+                <div
+                  className="flex items-center rounded-lg p-2 w-fit md:hidden"
+                  style={{ backgroundColor: getSubjectColor(subject) }}
+                >
+                  <Image
+                    src={`/icons/${subject}.svg`}
+                    alt={subject}
+                    width={18}
+                    height={18}
+                  />
+                </div>
+              </TableCell>
+
+              <TableCell>
+                <div className="flex items-center gap-2 w-full justify-end">
+                  <p className="text-2xl">
+                    {duration} <span className="max-md:hidden">mins</span>
+                  </p>
+
+                  <Image
+                    src="/icons/clock.svg"
+                    alt="minutes"
+                    width={14}
+                    height={14}
+                    className="md:hidden"
+                  />
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total</TableCell>
+            <TableCell className="text-right">$2,500.00</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </article>
+  );
+};
+
+export default CompanionsList;
